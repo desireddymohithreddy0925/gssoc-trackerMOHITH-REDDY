c206a533f837f847353218e45b2755ec704e56b3
PRODHOSH V.S <prodhosh3@gmail.com>
2026-06-27 15:27:19 +0530

    update with posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,8 +4,8 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  if (process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN) {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
       person_profiles: "identified_only", 
     });
